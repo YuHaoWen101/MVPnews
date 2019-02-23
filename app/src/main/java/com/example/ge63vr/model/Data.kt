aade7efd6acafd958277274082f7209f047c582a
YuHaoWen101 <43868539+YuHaoWen101@users.noreply.github.com>
2019-02-23 16:34:10 +0800

    规范了一点

--- app/src/main/java/com/example/ge63vr/model/Data.kt
+++ app/src/main/java/com/example/ge63vr/model/Data.kt
@@ -1,23 +1,23 @@
 package com.example.ge63vr.model
 data class Data(
-        val date: String,
-        val stories: List<StoriesBean>,
-        val top_stories: List<TopStoriesBean>
+    val date: String,
+    val stories: List<Story>,
+    val top_stories: List<TopStory>
 )
 
-data class StoriesBean(
-        val ga_prefix: String,
-        val id: Int,
-        val images: List<String>,
-        val multipic: Boolean,
-        val title: String,
-        val type: Int
+data class Story(
+    val ga_prefix: String,
+    val id: Int,
+    val images: List<String>,
+    val multipic: Boolean,
+    val title: String,
+    val type: Int
 )
 
-data class TopStoriesBean(
-        val ga_prefix: String,
-        val id: Int,
-        val image: String,
-        val title: String,
-        val type: Int
+data class TopStory(
+    val ga_prefix: String,
+    val id: Int,
+    val image: String,
+    val title: String,
+    val type: Int
 )
